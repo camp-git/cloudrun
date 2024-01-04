b747cb89419df6ff13b346d0458e3d9f9ed307b3
camp-git <pavan.ams14@gmail.com>
2024-01-04 11:00:26 -0500

    Add files via upload

---
+++ src/main/java/com/example/Cloud/run/CloudRunApplication.java
@@ -0,0 +1,13 @@
+package com.example.Cloud.run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudRunApplication.class, args);
+	}
+
+}
